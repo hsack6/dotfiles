1ccc15bc31d1b08d3a66ee7876df7985998f9ce7
yusuke <prsprivatestock@i.softbank.jp>
2018-09-10 22:04:43 +0900

    initial commit

---
+++ install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#======================================================
+#以下参考にしたページ
+#https://www.g104robo.com/entry/github-dotfiles
+#https://github.com/g104robo/dotfiles
+#======================================================
+
+packagelist=(
+#terminal
+     "gnome-terminal" 
+     "tmux"
+     "zsh"
+     #"terminator" 
+
+#開発環境
+     "build-essential"
+     "cmake"
+     "cmake-qt-gui"
+     "cmake-curses-gui"
+     "automake"
+     "git"
+     "vim"
+     "python3-pip"
+     "python-pip"
+
+#グラフ描画
+    "gnuplot"
+    "gnuplot-x11"
+
+)
+
+echo "start apt install apps..."
+for list in ${packagelist[@]}; do
+    sudo apt install -y ${list}
+done
+
+sudo apt update -y
+sudo apt upgrade -y
+
+./link.sh
